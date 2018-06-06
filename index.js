be0905f2c919b5d525f0d1e1eea21c75d61ed714
Charles Hatch <charlesghatch@gmail.com>
2018-06-06 15:28:22 -0600

    completed afternoon

--- index.js
+++ index.js
@@ -1,8 +1,69 @@
+require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0')
+const students = require('./students.json');
 
 const app = express();
 
+const {
+    SESSION_SECRET,
+    DOMAIN,
+    CLIENT_ID,
+    CLIENT_SECRET,
+    CALLBACK_URL
+} = process.env;
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new Auth0Strategy({
+    domain: DOMAIN,
+    clientID: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    callbackURL: CALLBACK_URL,
+    scope: 'opneid email profile'
+}, (accessToken, refreshToken, extraParams, profile, done) => {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    console.log(profile)
+    done(null, profile);
+}))
+
+passport.serializeUser((user, done) => {
+    done(null, { clientID: user.id, email: user._json.email, name: user._json.name })
+});
+passport.deserializeUser((obj, done) => {
+    done(null, obj)
+})
+
+
+//endpoint:
+app.get('/login', passport.authenticate('auth0',
+    { successRedirect: '/students', failureRedirect: '/login', connection: 'github' }
+));
+
+function authenticated(req, res, next) {
+    if(req.user){
+        next()
+    }else{
+        res.status(401).send('no bueno')
+    }
+}
+
+app.get('/students', authenticated, (req, res, next) => {
+    res.status(200).send(students)
+})
+
+
 
 const port = 3000;
-app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
+app.listen(port, () => { console.log(`Server listening on port ${port}`); });
